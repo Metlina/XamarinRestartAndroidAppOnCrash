04ccedbc141fdda60c003dafe7fd2f0201520e32
TINOPETRINA74E4\tinopetrina <tinopetrina@live.com>
2017-03-13 15:17:32 +0100

    Initial commit

---
+++ RestartAndroidAppTest/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Java.Lang;
+
+namespace RestartAndroidAppTest
+{
+    [Activity(Label = "RestartAndroidAppTest", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            MyApplication.activity = this;
+
+            //Thread.DefaultUncaughtExceptionHandler = new MyExceptionHandler(this);
+
+            if (Intent.GetBooleanExtra("crash", false))
+            {
+                Toast.MakeText(this, "App restarted after crash", ToastLength.Short).Show();
+            }
+
+            var button = FindViewById<Button>(Resource.Id.button);
+
+            button.Click += (sender, args) =>
+            {
+                int a = 1, b = 0, c;
+
+                c = a/b;
+            };
+        }
+    }
+}
+
